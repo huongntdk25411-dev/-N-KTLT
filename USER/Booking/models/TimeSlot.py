ff9d09d2359dc083c34d9d80cbbba82d3552745f
huongntdk25411-dev <huongntdk25411@gst.uel.edu.vn>
2026-03-03 22:49:45 +0700

    'new'

--- USER/Booking/models/TimeSlot.py
+++
@@ -1,4 +0,0 @@
-class TimeSlot:
-    def __init__(self, time, is_booked=False):
-        self.time = time
-        self.is_booked = is_booked
--- USER/Booking/test/test_2.py
+++
@@ -1,4 +0,0 @@
-from Final.timeDateEdit_Customers.FileFactory import FileFactory
-from Final.timeDateEdit_Customers.models.Booking import Booking
-ff=FileFactory()
-ff.readData("../dataset/database.json", Booking)
